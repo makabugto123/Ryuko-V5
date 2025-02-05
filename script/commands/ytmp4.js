de7d77959205b294b2fd36ac404d4e41a908e070
makabugto123 <139425756+makabugto123@users.noreply.github.com>
2025-02-05 08:38:00 +0800

    Update ytmp4.js

--- script/commands/ytmp4.js
+++ script/commands/ytmp4.js
@@ -34,7 +34,7 @@ module.exports.run = async function({ api, event, args }) {
     const channel = data1.channel;
     const duration = data1.duration;
     
-        const apiUrl = `https://downloader.iyot.plus/ytdl?url=${encodeURIComponent(yturl)}&type=mp4`;
+        const apiUrl = `https://downloader.iyot.plus/ytdl?url=${encodeURIComponent(yturl)}&type=mp4&resolution=360`;
     
         const response = await axios.get(apiUrl);
         const data = response.data;
